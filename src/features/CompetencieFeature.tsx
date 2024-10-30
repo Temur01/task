feb32cd33f9c95cb6c64149d2b10e0f753e307e8
Firuz Khamroev <fkhamroev@icloud.com>
2024-10-30 14:13:24 +0500

    fix:ui

--- src/features/CompetencieFeature.tsx
+++ src/features/CompetencieFeature.tsx
@@ -18,26 +18,35 @@ const CompetenciesSection = () => {
         <div className="w-2 h-6 bg-blue-600 mr-2"></div>
         <h2 className="text-2xl font-bold text-gray-900">Компетенцияларинг намоён бўлиши</h2>
       </div>
-      <div className="flex flex-col md:flex-row justify-between items-center mt-8">
-        <div className="grid grid-cols-2 md:grid-rows-2 md:grid-cols-3 gap-6">
+      <div className="flex flex-col gap-5 md:justify-between justify-center md:flex-row items-center mt-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 ">
           {competencies.map((item, index) => (
             <div key={index} className="flex items-center space-x-4">
-              <div style={{ width: 80, height: 80 }}>
+              <div >
                 <CircularProgressbar
-                  value={item.percentage}
-                  text={`${item.percentage}%`}
-                  styles={buildStyles({
-                    pathColor: item.color,
-                    textColor: "#333",
-                    trailColor: "#d1d5db",
-                  })}
+                     value={item.percentage}
+                      text={`${item.percentage}%`}
+                      styles={{
+                        ...buildStyles({
+                          pathColor: item.color,
+                          textColor: "#333",
+                          trailColor: "#d1d5db",
+                          
+                        }),
+                        root: {
+                          width: '70px', 
+                          height: '70px', 
+                        },
+                      }}
                 />
               </div>
               <span className="text-gray-800 font-semibold">{item.title}</span>
             </div>
           ))}
         </div>
-        <QRCodeSVG value="https://argos.uz" size={130} />
+        <div className="w-20 h-20 sm:w-24 sm:h-24 md:w-28 md:h-28 lg:w-32 lg:h-32">
+          <QRCodeSVG value="https://argos.uz" width="100%" height="100%" />
+        </div>
       </div>
       <p className="text-gray-500 text-center mt-4">Argos.uz 2024</p>
     </section>
